38bb4529d3d3cbca6c1c59d14dbfd19e4084fdc7
Jordan021 <jordanblue100@gmail.com>
2019-05-13 15:18:47 -0400

    Working on the form for booking the flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,6 +1,21 @@
 class FlightsController < ApplicationController
   
   def index 
+    @departure_airports = Airport.all.map{ |u| [u.name, u.id] }
+    @arrival_airports = Airport.all.map{ |u| [u.name, u.id] }
+    @passengers = [[1,1],[2,2],[3,3],[4,4]]
+    @last_date = Flight.maximum(:start).to_date
+    @flight_date = @first_date = Flight.minimum(:start).to_date 
+
+    if search = params[:flights]
+      @departure = search[:departure_airport_id]
+      @arrival   = search[:arrival_airport_id]
+      @leave_date = search[:leaving_date]
+      @passenger = search[:passengers]
+      
+      @search_result = Flight.departing(search[:departure_airport_id]).leave_day(
+                       search[:leaving_date])
+    end 
     
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,6 @@
 class Flight < ApplicationRecord
   belongs_to :arrival_airport, class_name: 'Airport'
   belongs_to :departure_airport, class_name: 'Airport'
+  scope :departing, -> (code) { where(departure_airport_id: code) }
+  scope :leave_day, -> (date) { where(start: date.to_date.all_day) }
 end
